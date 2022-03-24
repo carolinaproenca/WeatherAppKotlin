8008a00af7b6dcafcdd22c75ba1e7e57c1acb8fa
carolinaproenca <94008532+carolinaproenca@users.noreply.github.com>
2022-03-24 14:46:26 +0000

    Try days

--- app/src/main/java/com/example/android/application/models/Day.kt
+++ app/src/main/java/com/example/android/application/models/Day.kt
@@ -2,6 +2,4 @@ package com.example.android.application.models
 
 import java.time.LocalDateTime
 
-data class Day(var hours: List<Hour>, var date: LocalDateTime) {
-
-}
+data class Day(var hours: List<Hour>, var date: LocalDateTime)
--- app/src/main/java/com/example/android/application/weather/WeatherViewModel.kt
+++ app/src/main/java/com/example/android/application/weather/WeatherViewModel.kt
@@ -14,8 +14,12 @@ import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
+import java.util.*
 
-class WeatherViewModel : ViewModel(){
+class
+
+
+WeatherViewModel : ViewModel(){
 
     private val retrofit = WeatherApi.retrofitService
 
@@ -32,20 +36,23 @@ class WeatherViewModel : ViewModel(){
             try {
                 val listResult = retrofit.getProperties(city = "Porto", units = "metric")
 
-                val days = mutableListOf<Day>()
-                val arrayhour = arrayOf<Hour>()
+                var days = mutableListOf<Day>()
+                val arrayhour = arrayListOf<Hour>()
 
-            /*    arrayhour[0] = listResult.list
+                //days = listResult.list[0]
+                arrayhour[0] = listResult.list[0]
 
                 for(i in 0..arrayhour.size){
 
-                    val day : DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
-                    val parseday : LocalDateTime = LocalDateTime.parse(listResult.list[i].dtTxt, day)
+                    val day = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+                    val parseday = LocalDateTime.parse(listResult.list[i].dtTxt, day)
 
-                    if(arrayhour[i-1] != arrayhour[i])
-                        Day(arrayhour[i],parseday)
+                    if(getData() == parseday) // se o dia em que temos guardado (getData()) é igual ao dia que recebemos
+                        arrayhour.add(arrayhour[i]) // adicionamos ao array das horas a hora
+                    else// senão vamos criar um novo dia ao array de dias com a hora que temos e com o dia que recebemos
+                        days.add(Day(arrayhour,parseday))
 
-                }*/
+                }
 
                 _response.value = WeatherVM(listResult.city.name, listResult.list[0].main.temp.toString(),days)
 
@@ -53,5 +60,12 @@ class WeatherViewModel : ViewModel(){
                 smsError.value = "Failure+$e"
             }
         }
+    }
+
+    fun getData(){ // ver aqui qual é o dia em que estamos
+        val day = Day()
+        val currentDay
+        day.date = currentDay
+        return currentDay
     }
 }
